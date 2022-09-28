8377074b0d55e9011b008403e40cd92f8f08e432
Hemmat <mmhemat99@gmail.com>
2022-09-28 09:03:01 +0200

    edit

--- src/app/_service/company.service.ts
+++ src/app/_service/company.service.ts
@@ -8,19 +8,71 @@ export class CompanyService {
 
   constructor() { }
   Companies: Company[] = [
+
     {
+    id: 11,
+    name: "Amazon",
+    description: "Amazon company Amazon",
+
+    img1: `../../../assets/Assets/Brands/amazon.PNG`,
+    img2: "https://picsum.photos/18/300",
+    social_link: "cghh@amazon.com",
+    email: "Amazon@gmail.com",
+    hotline: '34897/983',
+    review: 44.85,
+    owner: { id: 11, name: "Dalia", image: "https://picsum.photos/10/300", email: "Dalia@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
+    ,
+    address: { id: 11, city: "sohag", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
+    ,
+    package: { id: 11, code: "Shopping_premium", cost: 50, tax: 404, fees: 5, name: "Shopping_premium", description: "Shopping_premium_description", company_type: "Shopping", currency: "dollar" }
+    },
+    {
+    id: 12,
+    name: "Aramex",
+    description: "Aramex company",
+
+    img1: `../../../assets/Assets/Brands/Aramex.PNG`,
+    img2: "https://picsum.photos/25/300",
+    social_link: "cghh@Aramex.com",
+    email: "Aramex@gmail.com",
+    hotline: '34897/983',
+    review: 44.85,
+    owner: { id: 12, name: "Aramex", image: "https://picsum.photos/178/300", email: "Aramex4@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
+    ,
+    address: { id: 12, city: "sohag", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
+    ,
+    package: { id: 12, code: "Shopping_premium", cost: 50, tax: 404, fees: 5, name: "Shopping_premium", description: "Shopping_premium_description", company_type: "Shopping", currency: "dollar" }
+    },
+    {
+      id: 13,
+      name: "Hitachi",
+      description: "Hitachi company",
+
+      img1: `../../../assets/Assets/Brands/Hitachi.PNG`,
+      img2: "https://picsum.photos/25/300",
+      social_link: "cghh@Hitachi.com",
+      email: "Hitachi@gmail.com",
+      hotline: '34897/983',
+      review: 44.85,
+      owner: { id: 13, name: "Basma", image: "https://picsum.photos/178/300", email: "Hitachi@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
+      ,
+      address: { id: 13, city: "sohag", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
+      ,
+      package: { id: 13, code: "Shopping_premium", cost: 50, tax: 404, fees: 5, name: "Shopping_premium", description: "Shopping_premium_description", company_type: "Shopping", currency: "dollar" }
+      }
+       ,{
       id: 1,
-      name: "apartment company",
-      description: "hvkhjkfddddddddddddddddddddddddddddddddddwsrtfethjg",
+      name: "facebook",
+      description: "facebookfacebookfacebookfacebookfacebook",
 
       img1: "https://picsum.photos/300/300",
       img2: "https://picsum.photos/200/300",
 
-      social_link: "cghh",
-      email: "vnbmnvn@gmail.com",
+      social_link: "cghh@facebook.com",
+      email: "facebook@gmail.com",
       hotline: '34897/983',
       review: 3.85,
-      owner: { id: 1, name: "Desomy", image: "http://forall.4all.ltd/uploads/image/111/16px/e7e0ced96ef9808b9ae1ca2029f5c5ce690743d2.webp", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996", mobile:"06541585" }
+      owner: { id: 1, name: "Desomy", image: "http://forall.4all.ltd/uploads/image/111/16px/e7e0ced96ef9808b9ae1ca2029f5c5ce690743d2.webp", email: "Desomy@orlydns.com", gender: 'male', birthdate: "14/11/1996", mobile:"06541585" }
       ,
       address: { id: 1, city: "Alex", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
       ,
@@ -28,17 +80,17 @@ export class CompanyService {
 },
     {
       id: 2,
-      name: "apartment ",
-      description: "hvkhjkfddddddddddddddddddddddddddddddddddwsrtfethjg",
+      name: "gmail ",
+      description: "gmailgmailgmailgmailgmailgmail",
 
       img1: "https://picsum.photos/400/300",
       img2: "https://picsum.photos/200/300",
 
-      social_link: "cghh",
-      email: "vnbmnvn@gmail.com",
+      social_link: "nvd,bn,d@gmail.com",
+      email: "gmail@gmail.com",
       hotline: '34897/983',
       review: 3.8522,
-      owner: { id: 2, name: "Desomy", image: "http://forall.4all.ltd/uploads/image/111/16px/e7e0ced96ef9808b9ae1ca2029f5c5ce690743d2.webp", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996", mobile:"06541585" }
+      owner: { id: 2, name: "Omer", image: "http://forall.4all.ltd/uploads/image/111/16px/e7e0ced96ef9808b9ae1ca2029f5c5ce690743d2.webp", email: "Omer@orlydns.com", gender: 'male', birthdate: "14/11/1996", mobile:"06541585" }
       ,
       address: { id: 2, city: "Alex", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
       ,
@@ -46,16 +98,16 @@ export class CompanyService {
 },
     {
       id: 3,
-      name: "office company",
-      description: "hvkhjkfddddddddddddddddddddddddddddddddddwsrtfethjg",
+      name: "Dailymotion",
+      description: "DailymotionDailymotionDailymotion",
 
       img1: "https://picsum.photos/500/300",
       img2: "https://picsum.photos/200/300",
-       social_link: "cghh",
-      email: "vnbmnvn@gmail.com",
+       social_link: "cghh@Dailymotion.com",
+      email: "Dailymotion@gmail.com",
       hotline: '34897/983',
       review: 34.85,
-      owner: { id:  110, name: "Desomy", image: "http://4all.ltd/uploads/image/110/16px/ca1d12d4e925ff0956104debb830f641697306d2.webp", email: "bifovol571@ploneix.com", gender: 'male', birthdate: "14/11/1996" , mobile:"+201099122112123"}
+      owner: { id:  110, name: "Abdo", image: "http://4all.ltd/uploads/image/110/16px/ca1d12d4e925ff0956104debb830f641697306d2.webp", email: "Abdo@ploneix.com", gender: 'male', birthdate: "14/11/1996" , mobile:"+201099122112123"}
       ,
       address: { id: 357, city: "Alex", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
       ,
@@ -63,16 +115,16 @@ export class CompanyService {
 },
     {
       id: 4,
-      name: "dsfsdf",
-      description: "hvkhjkfddddddddddddddddddddddddddddddddddwsrtfethjg",
+      name: "RadioTv",
+      description: "RadioTvRadioTvRadioTvRadioTvRadioTv",
 
       img1: "https://picsum.photos/600/300",
       img2: "https://picsum.photos/200/300",
-      social_link: "cghh",
-      email: "vnbmnvn@gmail.com",
+      social_link: "cghh@RadioTv.com",
+      email: "RadioTv@gmail.com",
       hotline: '34897/983',
       review: 34.85,
-      owner: { id: 4, name: "Desomy", image: "http://forall.4all.ltd/uploads/image/111/16px/e7e0ced96ef9808b9ae1ca2029f5c5ce690743d2.webp", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
+      owner: { id: 4, name: "Hend", image: "http://forall.4all.ltd/uploads/image/111/16px/e7e0ced96ef9808b9ae1ca2029f5c5ce690743d2.webp", email: "Hend@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
       ,
       address: { id: 4, city: "Alex", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
       ,
@@ -80,15 +132,15 @@ export class CompanyService {
 },
     {
       id: 5,
-      name: "dsfsdf",
-      description: "hvkhjkfddddddddddd",
+      name: "oper",
+      description: "oper",
       img1: "https://picsum.photos/700/300",
       img2: "https://picsum.photos/200/300",
-      social_link: "cghh",
-      email: "vnbmnvn@gmail.com",
+      social_link: "cghh@oper",
+      email: "oper@gmail.com",
       hotline: '34897/983',
       review: 34.85,
-      owner: { id: 1, name: "Desomy", image: "http://forall.4all.ltd/uploads/image/111/16px/e7e0ced96ef9808b9ae1ca2029f5c5ce690743d2.webp", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
+      owner: { id: 1, name: "Aya", image: "http://forall.4all.ltd/uploads/image/111/16px/e7e0ced96ef9808b9ae1ca2029f5c5ce690743d2.webp", email: "Aya@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
       ,
       address: { id: 1, city: "Alex", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
       ,
@@ -96,16 +148,16 @@ export class CompanyService {
 },
 {
 id: 7,
-name: "program company",
+name: "program ",
 description: "programming company sfffffffff",
 
 img1: "https://picsum.photos/100/300",
 img2: "https://picsum.photos/98/300",
 social_link: "cghh",
-email: "Haidy@gmail.com",
+email: "program@gmail.com",
 hotline: '34897/983',
 review: 12.85,
-owner: { id: 4, name: "Haidy", image: "https://picsum.photos/1000/300", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
+owner: { id: 4, name: "Haidy", image: "https://picsum.photos/1000/300", email: "Haidy@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
 ,
 address: { id: 4, city: "Asyut", street: 550, building: 34, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
 ,
@@ -113,16 +165,16 @@ package: { id: 4, code: "programming company", cost: 50, tax: 404, fees: 5, name
 },
 {
 id: 8,
-name: "C++ company",
+name: "C++company",
 description: "store ccccccccccccompany",
 
 img1: "https://picsum.photos/61/300",
 img2: "https://picsum.photos/91/300",
-social_link: "cghh",
-email: "Ahmed@gmail.com",
+social_link: "cghh@c++.com",
+email: "C++company@gmail.com",
 hotline: '34897/983',
 review: 10.85,
-owner: { id: 8, name: "Ahmed", image:"https://picsum.photos/13/300", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
+owner: { id: 8, name: "Ahmed", image:"https://picsum.photos/13/300", email: "Ahmed@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
 ,
 address: { id: 8, city: "cairo", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
 ,
@@ -130,16 +182,16 @@ package: { id: 8, code: "c++ code", cost: 50, tax: 404, fees: 5, name: "Shopping
 },
 {
 id: 9,
-name: "electric company",
+name: "electric ",
 description: "store company",
 
 img1: "https://picsum.photos/23/300",
 img2: "https://picsum.photos/75/300",
-social_link: "cghh",
-email: "Ali@gmail.com",
+social_link: "cghh@electric.com",
+email: "electric@gmail.com",
 hotline: '34897/983',
 review: 14.85,
-owner: { id: 9, name: "Ali", image:"https://picsum.photos/76/300", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
+owner: { id: 9, name: "Ali", image:"https://picsum.photos/76/300", email: "Ali@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
 ,
 address: { id: 9, city: "Alex", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
 ,
@@ -147,71 +199,20 @@ package: { id: 9, code: "Shopping_premium", cost: 50, tax: 404, fees: 5, name: "
 },
 {
 id: 10,
-name: "fooding company",
+name: "fooding",
 description: "fooding company",
 
 img1: "https://picsum.photos/600/300",
 img2: "https://picsum.photos/90/300",
-social_link: "cghh",
+social_link: "cghh@food.com",
 email: "Dina@gmail.com",
 hotline: '34897/983',
 review: 14.85,
-owner: { id: 10, name: "Dina", image:"https://picsum.photos/96/300", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
+owner: { id: 10, name: "Dina", image:"https://picsum.photos/96/300", email: "Dina@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
 ,
 address: { id: 10, city: "Aswan", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
 ,
 package: { id: 10, code: "Shopping_premium", cost: 50, tax: 404, fees: 5, name: "Shopping_premium", description: "Shopping_premium_description", company_type: "Shopping", currency: "dollar" }
-},
-{
-id: 11,
-name: "clothes company",
-description: "clothes company",
-
-img1: "https://picsum.photos/420/300",
-img2: "https://picsum.photos/18/300",
-social_link: "cghh",
-email: "Dalia@gmail.com",
-hotline: '34897/983',
-review: 44.85,
-owner: { id: 11, name: "Dalia", image: "https://picsum.photos/10/300", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
-,
-address: { id: 11, city: "sohag", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
-,
-package: { id: 11, code: "Shopping_premium", cost: 50, tax: 404, fees: 5, name: "Shopping_premium", description: "Shopping_premium_description", company_type: "Shopping", currency: "dollar" }
-},
-{
-id: 12,
-name: "shoes company",
-description: "shoes company",
-
-img1: "https://picsum.photos/920/300",
-img2: "https://picsum.photos/25/300",
-social_link: "cghh",
-email: "Basma@gmail.com",
-hotline: '34897/983',
-review: 44.85,
-owner: { id: 12, name: "Basma", image: "https://picsum.photos/178/300", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
-,
-address: { id: 12, city: "sohag", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
-,
-package: { id: 12, code: "Shopping_premium", cost: 50, tax: 404, fees: 5, name: "Shopping_premium", description: "Shopping_premium_description", company_type: "Shopping", currency: "dollar" }
-},
-{
-id: 13,
-name: "phones company",
-description: "phones company",
-
-img1: "https://picsum.photos/78/300",
-img2: "https://picsum.photos/68/300",
-social_link: "cghh",
-email: "MAhmoud@gmail.com",
-hotline: '34897/983',
-review: 44.85,
-owner: { id: 13, name: "MAhmoud", image: "https://picsum.photos/173/300", email: "cotoso3354@orlydns.com", gender: 'male', birthdate: "14/11/1996" , mobile:"06541585"}
-,
-address: { id: 13, city: "sohag", street: 50, building: 404, intercom: "NA", floor: "ffffff", detailed_address: "dddddd" }
-,
-package: { id: 13, code: "Shopping_premium", cost: 50, tax: 404, fees: 5, name: "Shopping_premium", description: "Shopping_premium_description", company_type: "Shopping", currency: "dollar" }
 },
 ];
 
